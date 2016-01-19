b1854578093ad9b22e581c9470eb47f661daabe2
lyx0206331 <rq880113@163.com>
2016-01-19 18:16:12 +0800

    add umeng analytics

--- app/src/main/java/com/adrian/games/activity/BaseActivity.java
+++ app/src/main/java/com/adrian/games/activity/BaseActivity.java
@@ -5,11 +5,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.umeng.analytics.game.UMGameAgent;
+
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        UMGameAgent.setDebugMode(true);//设置输出运行时日志
+        UMGameAgent.init(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        UMGameAgent.onPause(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        UMGameAgent.onResume(this);
     }
 
     protected void openActivity(Class<? extends Activity> cls) {
