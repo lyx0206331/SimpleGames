0825c9c441e3d2c6ca038478365609396210dc3f
lyx0206331 <rq880113@163.com>
2016-01-19 17:08:49 +0800

    add ai gobang

--- app/src/main/java/com/adrian/games/activity/MainActivity.java
+++ app/src/main/java/com/adrian/games/activity/MainActivity.java
@@ -5,13 +5,17 @@ import android.view.View;
 import android.widget.Button;
 
 import com.adrian.games.R;
+import com.adrian.games.gobang.AutoGobangActivity;
 import com.adrian.games.gomoku.GomokuMainActivity;
 import com.adrian.games.mine.MineMainActivity;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
+    public static boolean playerFirst = true;
+
     private Button mMineBtn;
     private Button mGomokuBtn;
+    private Button mAutoGobangBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +29,13 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private void initView() {
         mMineBtn = (Button) findViewById(R.id.btn_mine);
         mGomokuBtn = (Button) findViewById(R.id.btn_gomoku);
+        mAutoGobangBtn = (Button) findViewById(R.id.btn_auto_gobang);
     }
 
     private void setListener() {
         mMineBtn.setOnClickListener(this);
         mGomokuBtn.setOnClickListener(this);
+        mAutoGobangBtn.setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +46,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.btn_gomoku:
                 openActivity(GomokuMainActivity.class);
+                break;
+            case R.id.btn_auto_gobang:
+                openActivity(AutoGobangActivity.class);
                 break;
         }
     }
--- app/src/main/java/com/adrian/games/gomoku/GomokuMainActivity.java
+++ app/src/main/java/com/adrian/games/gomoku/GomokuMainActivity.java
@@ -2,14 +2,20 @@ package com.adrian.games.gomoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.adrian.games.R;
 
 public class GomokuMainActivity extends AppCompatActivity {
 
+    private GomokuView gbv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gomoku_main);
+
+        gbv = (GomokuView) this.findViewById(R.id.gobangview);
+        gbv.setTextView((TextView) this.findViewById(R.id.text));
     }
 }
